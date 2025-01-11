7ac4fc7d5336d65f389ee0cd03d3578a3fa10614
Murugan Padmanaban <pm.murugan@gmai.com>
2025-01-11 15:54:13 +0000

    Committing the change

---
+++ functions.py
@@ -0,0 +1,13 @@
+#declare a tupe
+my_tuple = (10, 20, 30, 40, 50)
+my_list = [10, 20, 30, 40, 50]
+my_dictionary = {
+    "name": "Alice",
+    "age": "25",
+    "city": "New York"
+}
+
+print("First Element:", my_tuple[0])
+print("Second Element:", my_tuple[1])
+
+  
